b0446486277a8411f2477bb63ee7d3e58a033c85
ba1ah <118501502+ba1ah@users.noreply.github.com>
2024-04-19 19:01:52 +0530

    Update play.py

--- DAXXMUSIC/utils/inline/play.py
+++ DAXXMUSIC/utils/inline/play.py
@@ -65,7 +65,7 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        
+        [InlineKeyboardButton(text="ᴛᴀᴋᴇ ᴍᴇ ᴅᴇᴀʀ", url=f"https://t.me/{app.username}?startgroup=true")],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
